953bda1099074949289a5b8fe7275f53d0369a65
RanolP <me@ranolp.dev>
2024-01-09 22:03:32 +0900

    upgrade solid start & ready to deploy in cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
+    start: {
+        solid: undefined as any,
+        server: {
+            preset: 'cloudflare-pages'
+        }
+    },
     plugins: [
         UnoCSS(),
     ]
