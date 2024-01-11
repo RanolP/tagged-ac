5ef5d81aa08b8d98aa0c128646b4a19947c5b0cf
RanolP <me@ranolp.dev>
2024-01-12 02:25:35 +0900

    upgrade typescript

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,8 @@ import type { AttributifyAttributes } from 'unocss/preset-attributify';
 
 declare module 'solid-js' {
   namespace JSX {
-    interface HTMLAttributes<T> extends AttributifyAttributes {}
+    interface HTMLAttributes<T> extends AttributifyAttributes {
+      lh?: string;
+    }
   }
 }
