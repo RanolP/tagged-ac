8f1fdef641d56f68cdd787441bdb950ceedfa65a
RanolP <me@ranolp.dev>
2024-01-13 21:24:23 +0900

    add logo and readme renewal

--- src/routes/index.page.tsx
+++ src/routes/index.page.tsx
@@ -1,3 +1,8 @@
 export default function IndexPage() {
-  return <div>Hello guys</div>;
+  return (
+    <div text-16 font-mono>
+      <img src="tagged-ac-logo.png" w="[360px]" />
+      tagged.AC
+    </div>
+  );
 }
