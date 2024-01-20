4867df587b0855137b5a58a184d6a8a33b954ce4
RanolP <me@ranolp.dev>
2024-01-20 19:12:07 +0900

    change rule name

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import type { AttributifyAttributes } from 'unocss/preset-attributify';
 
 declare module 'solid-js' {
   export namespace JSX {
-    // eslint-disable-next-line no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     export interface HTMLAttributes<T> extends AttributifyAttributes {
       lh?: string;
     }
