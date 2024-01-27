18addcf61f2e6a0825e68ce6bdf8045b9fe9df4f
RanolP <me@ranolp.dev>
2024-01-27 22:55:47 +0900

    now we can list rooms

--- src/routes/api/(server-list)/servers/route.ts
+++ src/routes/api/(server-list)/servers/route.ts
@@ -1,1 +1,16 @@
-export function GET() {}
+import dayjs from 'dayjs';
+import { gte } from 'drizzle-orm';
+
+import { advertisement } from '~/server/database/schema';
+
+import { defineApiRoute } from '../../_util/define-api-route';
+
+export const GET = defineApiRoute({}, async ({ db }) => {
+  return await db
+    .select({
+      id: advertisement.id,
+    })
+    .from(advertisement)
+    .where(gte(advertisement.valid_until, dayjs().toISOString()))
+    .execute();
+});
