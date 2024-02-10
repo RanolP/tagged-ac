c5d8dde2e1e1d93b8d07fe1eadb3c43f76cf823e
RanolP <me@ranolp.dev>
2024-02-11 03:47:03 +0900

    ok no context please ha.....

--- src/features/terminal/history-context.tsx
+++ src/features/terminal/history-context.tsx
@@ -1,37 +1,39 @@
 import {
-  createContext,
+  // createContext,
   createSignal,
   JSX,
-  ParentProps,
-  Signal,
-  useContext,
+  // ParentProps,
+  // Signal,
+  // useContext,
 } from 'solid-js';
 
-const TerminalHistoryContext = createContext<Signal<JSX.Element[]>>();
+// Why the solid-start doesn't properly support this context? garbage.
+// const TerminalHistoryContext = createContext<Signal<JSX.Element[]>>();
 
-export const TerminalHistoryProvider = ({ children }: ParentProps) => {
-  const historySignal = createSignal<JSX.Element[]>([]);
-  return (
-    <TerminalHistoryContext.Provider value={historySignal}>
-      {children}
-    </TerminalHistoryContext.Provider>
-  );
-};
+// export const TerminalHistoryProvider = ({ children }: ParentProps) => {
+const historySignal = createSignal<JSX.Element[]>([]);
+//   return (
+//     <TerminalHistoryContext.Provider value={historySignal}>
+//       {children}
+//     </TerminalHistoryContext.Provider>
+//   );
+// };
 
 export const useTerminalHistoryContext = () => {
-  const value = useContext(TerminalHistoryContext);
-  if (!value) {
-    throw new Error(
-      'useTerminalHistoryContext must be called inside TerminalHistoryContext',
-    );
-  }
-  return value ?? [null, null];
+  // const value = useContext(TerminalHistoryContext);
+  // if (!value) {
+  //   throw new Error(
+  //     'useTerminalHistoryContext must be called inside TerminalHistoryContext',
+  //   );
+  // }
+  // return value ?? [null, null];
+  return historySignal;
 };
 
 export function useEcho() {
-  const historySignal = useContext(TerminalHistoryContext);
+  // const historySignal = useContext(TerminalHistoryContext);
   return (e: JSX.Element) => {
-    if (!historySignal) return;
+    // if (!historySignal) return;
     const [, setHistories] = historySignal;
     setHistories((prev) => [...prev, e]);
   };
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,20 +1,20 @@
 import { RouteSectionProps } from '@solidjs/router';
 
 import { StatusBar } from '~/features/status-bar';
-import { TerminalHistoryProvider } from '~/features/terminal';
 
+// import { TerminalHistoryProvider } from '~/features/terminal';
 import { Background } from './_components/background';
 
 export default function Layout(props: RouteSectionProps) {
   return (
-    <TerminalHistoryProvider>
-      <div lh-normal font-mono>
-        <Background />
-        <div relative z-1>
-          <StatusBar />
-          {props.children}
-        </div>
+    // <TerminalHistoryProvider>
+    <div lh-normal font-mono>
+      <Background />
+      <div relative z-1>
+        <StatusBar />
+        {props.children}
       </div>
-    </TerminalHistoryProvider>
+    </div>
+    // </TerminalHistoryProvider>
   );
 }
